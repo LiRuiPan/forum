1fbb615b8c83bf330f0794d15abd92c480e9627e
liruipan <326069973@qq.com>
2019-01-21 21:06:03 +0800

    实现token

--- routes/helper.py
+++ routes/helper.py
@@ -5,6 +5,9 @@ from functools import wraps
 from flask import session, request, abort, redirect, url_for
 from models.user import User
 from utils import log
+import redis
+
+cache = redis.StrictRedis()
 
 
 def current_user():
@@ -27,3 +30,37 @@ def login_required(route_function):
             return route_function()
 
     return f
+
+
+def csrf_required(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        token = request.args['token']
+        user_id = request.args.get('user_id', '')
+        if user_id == '':
+            u = current_user()
+            user_id = str(u.id)
+        else:
+            user_id = str(user_id)
+
+        if cache.exists(token) and cache.get(token).decode() == user_id:
+            cache.delete(token)
+            return f(*args, **kwargs)
+        else:
+            abort(401)
+
+    return wrapper
+
+
+def new_csrf_token(*user_id):
+    if len(user_id) == 0:
+        u = current_user()
+        if u is None:
+            user_id = str(-1)
+        else:
+            user_id = str(u.id)
+    else:
+        user_id = str(user_id[0])
+    token = str(uuid.uuid4())
+    cache.set(token, user_id)
+    return token
