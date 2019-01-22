337f5a18d74ffcffe4e68fd04a12d187ddfe1821
liruipan <326069973@qq.com>
2019-01-22 19:56:29 +0800

    实现管理员后台删帖功能

--- routes/admin.py
+++ routes/admin.py
@@ -9,6 +9,9 @@ from flask import (
 from routes.helper import admin_required, current_user, new_csrf_token, csrf_required
 
 from models.board import Board
+from models.topic import Topic
+from models.message import Messages
+from models.reply import Reply
 
 main = Blueprint('admin', __name__)
 
@@ -27,4 +30,24 @@ def index():
 def add_board():
     form = request.form
     Board.new(form)
+    return redirect(url_for('.index'))
+
+
+@main.route("/delete_topic", methods=["POST", "GET"])
+@admin_required
+@csrf_required
+def delete_topic():
+    topic_title = request.form['title']
+    t = Topic.one(title=topic_title)
+    topic_id = t.id
+    user_id = t.user_id
+    content = '您的帖子《{}》已被管理员删除'.format(t.title)
+    form = dict(
+        receiver_id=user_id,
+        sender_id=current_user().id,
+        content=content
+    )
+    Messages.new(form)
+    Topic.delete(id=topic_id)
+    Reply.delete(topic_id=topic_id)
     return redirect(url_for('.index'))
